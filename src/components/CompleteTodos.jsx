444b6e31dc851653a708249abba20342bb88d514
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2022-05-01 21:38:56 +0900

    Initial commit

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,25 @@
+export const CompleteTodos = ({ todos, onClickBack }) => {
+  return (
+    <>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {todos.map((todo, index) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button
+                  onClick={() => {
+                    onClickBack(index);
+                  }}
+                >
+                  戻す
+                </button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
